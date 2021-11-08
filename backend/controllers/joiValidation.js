fca0b6b8021b29d6484293e202f83386fc62f43a
Sharif_Hosafy <Sherifismail44@gmail.com>
2021-11-08 10:03:12 +0200

    refactoring file structure

---
+++ backend/controllers/joiValidation.js
@@ -0,0 +1,42 @@
+const Joi = require("joi");
+
+const userRegisterValidationSchema = (data) => {
+  const schema = Joi.object({
+    username: Joi.string().min(3).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+    phone: Joi.number().min(6).required(),
+    location: Joi.string().min(6).required(),
+    type: Joi.string().required(),
+    description: Joi.string().min(6),
+    tailorType: Joi.string().min(3),
+  });
+  return schema.validate(data);
+};
+
+const userLoginValidationSchema = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required(),
+    type: Joi.string().required(),
+  });
+  return schema.validate(data);
+};
+
+const userUpdateValidationSchema = (data) => {
+  const schema = Joi.object({
+    username: Joi.string().min(6),
+    email: Joi.string().min(6).email(),
+    location: Joi.string(),
+    phone: Joi.number().min(6),
+    description: Joi.string().min(6),
+    tailorType: Joi.string().min(3),
+  });
+  return schema.validate(data);
+};
+
+module.exports = {
+  userRegisterValidationSchema,
+  userLoginValidationSchema,
+  userUpdateValidationSchema,
+};
---
+++ backend/routes/vendor.js
@@ -0,0 +1,64 @@
+const router = require("express").Router();
+const Vendor = require("../models/Vendor");
+const verify = require("../controllers/verifyToken");
+const { userUpdateValidationSchema } = require("../controllers/joiValidation");
+
+//get only one vendor to be accessed from any other users
+router.get("/:id", verify, async (req, res, next) => {
+  try {
+    const getOneVendor = await Vendor.findById(req.params.id);
+    res.json(getOneVendor);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//vendor Profile only accessed by himself or Admin
+router.get("/:id/profile", verify, async (req, res, next) => {
+  try {
+    if (req.params.id !== req.user._id || req.user.type !== "vendor")
+      throw new Error("Youd don't have permission");
+    const getVendorProfile = await Vendor.findById(req.user._id);
+    res.json(getVendorProfile);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// update Vendor info only accessed by himself or Admin
+router.patch("/:id/profile", verify, async (req, res, next) => {
+  // validation for the info to be updated
+  const { error } = userUpdateValidationSchema(
+    req.body
+  );
+  if (error) return next(error.details[0]);
+
+  try {
+    if (req.params.id !== req.user._id || req.user.type !== "vendor")
+      throw new Error("Youd don't have permission"); ////// Vendor can only update himself or Admin
+
+    const updates = req.body;
+    const updatedVendor = await Vendor.findByIdAndUpdate(req.user._id, updates);
+    if (!updatedVendor) return next(); // if the user does not exist
+    res.json(updatedVendor);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// delete Vendor only accessed by himself or Admin
+router.delete("/:id/profile", verify, async (req, res, next) => {
+  // we need to delete token from client to make sure not returning null values
+  try {
+    if (req.params.id !== req.user._id || req.user.type !== "vendor")
+      throw new Error("Youd don't have permission"); ////// Vendor can only delete himself or Admin
+
+    const deletedVendor = await Vendor.findByIdAndDelete(req.user._id);
+    if (!deletedVendor) return next(); // if the user does not exist
+    res.json(deletedVendor);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
